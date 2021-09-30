dcb9edda3a329e2cba86bddefd0a909081a09cbe
Evelin Merino-Torres <em303258@gmail.com>
2021-09-30 09:30:32 -0400

    Comment

--- Lab - Classes.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
+++ Lab - Classes.playground/Pages/1. Exercise - Define a Base Class.xcplaygroundpage/Contents.swift
@@ -5,12 +5,32 @@
  
  Create a `Spaceship` class with three variable properties: `name`, `health`, and `position`. The default value of `name` should be an empty string and `health` should be 0. `position` will be represented by an `Int` where negative numbers place the ship further to the left and positive numbers place the ship further to the right. The default value of `position` should be 0.
  */
-
+class Spaceship {
+    var name: String = " "
+    var health: Int = 0
+    var position: Int = 0
+    
+    func moveLeft() {
+        position -= 1
+    }
+    
+    func moveRight() {
+        position += 1
+    }
+    
+    func wasHit() {
+        health -= 5
+        if health <= 0 {
+            print("Sorry, your ship was hit too many times. Do you want to play again")
+        }
+    }
+}
 
 /*:
  Create a `let` constant called `falcon` and assign it to an instance of `Spaceship`. After initialization, set `name` to "Falcon".
  */
-
+let falcon = Spaceship()
+falcon.name = "Falcon"
 
 /*:
  Go back and add a method called `moveLeft()` to the definition of `Spaceship`. This method should adjust the position of the spaceship to the left by one. Add a similar method called `moveRight()` that moves the spaceship to the right. Once these methods exist, use them to move `falcon` to the left twice and to the right once. Print the new position of `falcon` after each change in position.
